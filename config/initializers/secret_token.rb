747cdd53b7a1e12634a4c29c99673b510e581c58
Or Peled <orppeled@gmail.com>
2013-03-28 00:48:15 +0200

    First Commit
    Good Luck

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FarmBowWebsite::Application.config.secret_token = 'b302e1a20a17e13b71cc46684520f61e75a5136874bdb6ca41a361e0e1f7bacdd7ce15cc0c4bcd1dac11d9bba28b6fda3cc83c4a25a002a684f007644c58834e'
